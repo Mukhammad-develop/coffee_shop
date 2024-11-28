c4f71cb30a8ac90c79cd6ba85a2dc6aebc6947c3
Mukhammad-develop <115652500+Mukhammad-develop@users.noreply.github.com>
2024-11-29 03:56:15 +0500

    swagger

--- coffee_shop_backend/coffee_shop_backend/urls.py
+++ coffee_shop_backend/coffee_shop_backend/urls.py
@@ -1,10 +1,26 @@
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+schema_view = get_schema_view(
+    openapi. Info(
+        title="Coffee Shop Swagger Documendation",
+        default_version='v1',
+        description="Test description",
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('core.urls')),
